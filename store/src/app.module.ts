aab3be66e98dae3859ca8caa971a25c7ff8b564f
Denys Chepiha <dchepiha@exoft.net>
2023-03-05 13:37:38 +0100

    Describe all models of app

--- store/src/app.module.ts
+++ store/src/app.module.ts
@@ -4,10 +4,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 // Services
 import { AppService } from './app.service';
+// Modules
+import { AuthModule } from './auth/auth.module';
+import { TopPageModule } from './top-page/top-page.module';
+import { ProductModule } from './product/product.module';
+import { ReviewModule } from './review/review.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [AuthModule, TopPageModule, ProductModule, ReviewModule],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
